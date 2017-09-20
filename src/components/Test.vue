4527b6c4110fd1828c2716d2fd1b251936221e1a
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-09-20 18:44:07 +0800

    Added test component

---
+++ src/components/Test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="title"><br>
+    <h1>{{ title }}</h1>
+    <p v-if="showName">{{ user.fname }}</p>
+    <p v-else>Nobody</p>
+    <p>{{ user.lname }}</p>
+    <ul>
+      <li
+        v-for="item in items"
+        v-bind:key="item.id">{{ item.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      title: 'Hello world',
+      user: {
+        fname: 'Jefferson',
+        lname: 'Rylee'
+      },
+      showName: true,
+      items: [
+        { id: 1, title: 'Apple' },
+        { id: 2, title: 'Banana' },
+        { id: 3, title: 'Orange' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
