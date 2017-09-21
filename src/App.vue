74c978bff4ede8f675a2f17e5a0a05f22101138d
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-09-21 08:22:07 +0800

    User contacts app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <test msg="Hello"></test>
+    <users></users>
   </div>
 </template>
 
 <script>
 import Test from './components/Test'
+import Users from './components/Users'
 
 export default {
   name: 'app',
   components: {
-    Test
+    Test,
+    Users
   }
 }
 </script>
---
+++ src/components/Users.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="users">
+    <h1>Users</h1>
+      <form v-on:submit="addUser">
+
+        <div>
+          <label for="name">Name</label>
+          <input type="text" name="name" id="name" v-model="newUser.name" placeholder="Enter Name">
+        </div>
+
+        <div>
+          <label for="email">Email</label>
+          <input type="text" name="email" id="email" v-model="newUser.email" placeholder="Enter Email">
+        </div>
+
+        <div>
+          <input type="submit" value="Submit">
+        </div>
+
+      </form>
+    <ul>
+      <li v-bind:key="user.id" v-for="user in users">
+        <input type="checkbox" class="toggle" v-model="user.contacted">
+        <span :class="{contacted: user.contacted}">
+          {{ user.name }} {{ user.email }}
+          <button v-on:click="deleteUser(user)">x</button>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  props: {
+    
+  },
+  data() {
+    return {
+      newUser: {},
+      users: [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'johndoe@mail.com',
+          contacted: false
+        },
+        {
+          id: 2,
+          name: 'Jane Doe',
+          email: 'janedoe@mail.com',
+          contacted: false
+        },
+        {
+          id: 3,
+          name: 'John Smith',
+          email: 'johnsmith@mail.com',
+          contacted: false
+        },
+      ]
+    }
+  },
+  methods: {
+    addUser: function(e) {
+      this.users.push({
+        name: this.newUser.name,
+        email: this.newUser.email,
+        contacted: false
+      })
+      e.preventDefault()
+    },
+    deleteUser: function(user) {
+      this.users.splice(this.users.indexOf(user), 1)
+    }
+  },
+  computed: {
+    
+  }
+}
+</script>
+
+<style scoped>
+.contacted {
+  text-decoration: line-through
+}
+</style>
